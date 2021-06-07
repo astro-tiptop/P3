23e1e7fe3968e9e8c44917acbbe73c2f3425f3ca
Alexis Lau <alexis.yslau@gmail.com>
2021-06-07 22:36:39 +0200

    Suggesting to add a setup.py file for a proper installation.

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages
+
+setup(name='P3',
+      version='0.0.0beta',
+      url='https://github.com/oliviermartin-lam/P3.git',
+      packages=find_packages())
