1be28667102d66adc8c690f0146e19547e59edac
FabioRossiArcetri <rossi76@gmail.com>
2025-02-05 12:31:05 +0100

    (FRO) Removing unnecessary prints.

--- p3/aoSystem/fourierModel.py
+++ p3/aoSystem/fourierModel.py
@@ -1522,5 +1522,4 @@ class fourierModel:
 
 import pathlib
 file_ini0 = str(pathlib.Path(__file__).parent.parent.parent.resolve()) + '/data/dummy.ini'
-print(file_ini0)
 faoDummy = fourierModel(path_ini=file_ini0, calcPSF=False, verbose=False, display=False, path_root="", doComputations=True)
