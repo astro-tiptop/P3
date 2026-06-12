88a535f850b67fec494185598c3054c96c179c7e
guidoagapito <guido.agapito@inaf.it>
2026-06-12 19:28:27 +0200

    (GA) P3_GPU_WARMUP

--- p3/aoSystem/fourierModel.py
+++ p3/aoSystem/fourierModel.py
@@ -2465,8 +2465,10 @@ class fourierModel:
         print("="*70 + "\n")
 
 import pathlib
-file_ini0 = str(pathlib.Path(__file__).parent.parent.absolute()) + '/dummy.ini'
-faoDummy = fourierModel(path_ini=file_ini0, calcPSF=False, verbose=False, display=False,
-                        path_root="", doComputations=True, computeFocalAnisoCov=False,
-                        reduce_memory=True)
-faoDummy = None
+# Conditional GPU warmup based on environment variable
+if os.environ.get('P3_GPU_WARMUP', 'FALSE').upper() == 'TRUE':
+    file_ini0 = str(pathlib.Path(__file__).parent.parent.absolute()) + '/dummy.ini'
+    faoDummy = fourierModel(path_ini=file_ini0, calcPSF=False, verbose=False, display=False,
+                            path_root="", doComputations=True, computeFocalAnisoCov=False,
+                            reduce_memory=True)
+    faoDummy = None
